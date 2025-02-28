c28db2a1b478769d0db62bce62ff0e1b3c62edb4
BluePudding0913 <umi2003256@gmail.com>
2025-02-28 17:20:31 +0900

    Delete H0tsp0t directory

--- H0tsp0t/loop.ps1
+++
@@ -1,9 +0,0 @@
-Start-Sleep -s 21600
-while ($true) {
-    Get-Date
-    echo "This is running to keep the state of MobileHotspot. "
-    echo "If possible, please refrain from removing this window."
-    echo "(written by Asaumi)"
-    powershell -NoProfile -ExecutionPolicy Unrestricted .\run.ps1
-    Start-Sleep -s 61100
-}
--- H0tsp0t/open_window.ps1
+++
@@ -1,2 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-Start-Process -FilePath "ms-settings:network-mobilehotspot"
